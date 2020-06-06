0f6a695f7d38abb1798f5c06b956a9a657af1a91
eniepa <apeine.k@gmail.com>
2020-06-06 18:52:27 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DigitalClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MainTimer = new System.Windows.Forms.Timer(this.components);
+            this.LabelTimer = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MainTimer
+            // 
+            this.MainTimer.Tick += new System.EventHandler(this.MainTimer_Tick);
+            // 
+            // LabelTimer
+            // 
+            this.LabelTimer.AutoSize = true;
+            this.LabelTimer.Font = new System.Drawing.Font("Yu Gothic UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.LabelTimer.Location = new System.Drawing.Point(53, 62);
+            this.LabelTimer.Name = "LabelTimer";
+            this.LabelTimer.Size = new System.Drawing.Size(421, 86);
+            this.LabelTimer.TabIndex = 0;
+            this.LabelTimer.Text = "00:00:00:00:0";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.buttonStart.Location = new System.Drawing.Point(52, 205);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(137, 49);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.buttonStop.Location = new System.Drawing.Point(337, 205);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(137, 49);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.buttonPause.Location = new System.Drawing.Point(194, 205);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(137, 49);
+            this.buttonPause.TabIndex = 3;
+            this.buttonPause.Text = "pause";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 297);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.LabelTimer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer MainTimer;
+        private System.Windows.Forms.Label LabelTimer;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonPause;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalClock
+{
+    public partial class Form1 : Form
+    {
+        int dsec = 0;
+        int sec = 0;
+        int min = 0;
+        int hou = 0;
+        int day = 0;
+        string timerValue = null;
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Black;
+            LabelTimer.ForeColor = Color.White;
+        }
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            dsec += 1;
+            if (dsec == 10)
+            {
+                dsec = 0;
+                sec += 1;
+            }
+            if (sec == 60)
+            {
+                sec = 0;
+                min += 1;
+            }
+            if (min == 60)
+            {
+                min = 0;
+                hou += 1;
+            }
+            if (hou == 24)
+            {
+                hou = 0;
+                day += 1;
+            }
+            timerValue = day.ToString("00") + ":"+
+                         hou.ToString("00") + ":" +
+                         min.ToString("00") + ":" +
+                         sec.ToString("00") + ":" +
+                         dsec.ToString("0");
+            LabelTimer.Text = timerValue;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            MainTimer.Start();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            MainTimer.Stop();
+            day = 0;
+            hou = 0;
+            min = 0;
+            sec = 0;
+            dsec = 0;
+            timerValue = day.ToString("00") + ":" +
+             hou.ToString("00") + ":" +
+             min.ToString("00") + ":" +
+             sec.ToString("00") + ":" +
+             dsec.ToString("0");
+            LabelTimer.Text = timerValue;
+
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            MainTimer.Stop();
+        }
+    }
+}
